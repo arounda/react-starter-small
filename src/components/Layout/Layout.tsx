858f27af44b2e2e1170e3eaf99df8be6735ca9a0
Max <webjump9@gmail.com>
2024-03-21 10:12:18 +0200

    layout update

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 
 import { SearchParamsContextProvider } from '@/context/searchParams';
 import { StoreContextProvider } from '@/context/store';
 
-import { Content } from './components/Content';
+import { Footer } from '../Common/Footer/Footer';
+import { Header } from '../Common/Header/Header';
 
 export const Layout: FC = () => {
   return (
     <SearchParamsContextProvider>
       <StoreContextProvider>
-        <Content />
+        <Header />
+        <Outlet />
+        <Footer />
       </StoreContextProvider>
     </SearchParamsContextProvider>
   );
